5de15ec616ef3ca0bb9ad0f22a4f0f53164e5d20
simon01092024 <simonkudus510@gmail.com>
2024-04-09 20:53:22 -0700

    add home + about

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+# Define the home view
+
+
+def home(request):
+    # Include an .html file extension - unlike when rendering EJS templates
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
