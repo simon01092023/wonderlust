621624a0ab4e23167b57306c92cd823091838ab4
Lilysaria <bloomyday56@gmail.com>
2024-04-10 14:29:28 -0700

    add PostCard functionalities

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import PostCard, Location
 # Register your models here.
+
+admin.site.register(PostCard)
+admin.site.register(Location)
--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('postcards/', views.postcards_index, name='index'),
+    path('postcards/<int:postcard_id>/', views.postcards_detail, name='detail'),
+    path('postcards/create/', views.PostCardCreate.as_view(), name='postcards_create'),
+    path('postcards/<int:pk>/update/', views.PostCardUpdate.as_view(), name='postcards_update'),
+    path('postcards/<int:pk>/delete/', views.PostCardDelete.as_view(), name='postcards_delete'),
 ]
