ee78f85b00c4aee7bf7e900daef6a8c63aa04b95
simon01092024 <simonkudus510@gmail.com>
2024-04-12 09:12:14 -0700

    map visual add

--- main_app/models.py
+++ main_app/models.py
@@ -2,26 +2,35 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django_google_maps import fields as map_fields
+
 
 class Location(models.Model):
     name = models.CharField(max_length=100)
-    zipcode = models.CharField(max_length=200,blank=True, null=True)
-    city = models.CharField(max_length=200,blank=True, null=True)
-    country = models.CharField(max_length=200,blank=True, null=True)
-    address = models.CharField(max_length=200,blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True,blank=True, null=True)
+    zipcode = models.CharField(max_length=200, blank=True, null=True)
+    city = models.CharField(max_length=200, blank=True, null=True)
+    country = models.CharField(max_length=200, blank=True, null=True)
+    address = models.CharField(max_length=200, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     edited_at = models.DateTimeField(auto_now=True)
     # latitude = models.FloatField(validators=[MinValueValidator(-90),MaxValueValidator(90)])
     # longitude = models.FloatField(validators=[MinValueValidator(-180),MaxValueValidator(180)])
+
     def __str__(self):
         return self.name
 
 
+class Map(models.Model):
+    address = map_fields.AddressField(max_length=200)
+    geolocation = map_fields.GeoLocationField(max_length=100)
+
+    def __str__(self):
+        return self.address
 
 
 class PostCard(models.Model):
     title = models.CharField(max_length=100,)
-    date = models.DateField(("PostDate"), auto_now=False, auto_now_add=False)   
+    date = models.DateField(("PostDate"), auto_now=False, auto_now_add=False)
     content = models.TextField(max_length=250)
     locations = models.ManyToManyField(Location)
 
@@ -30,17 +39,14 @@ class PostCard(models.Model):
 
     def __str__(self):
         return self.title
-    
+
     def get_absolute_url(self):
         return reverse("detail", kwargs={"postcard_id": self.id})
-    
+
 
 class Photo(models.Model):
     url = models.CharField(max_length=200)
     postcard = models.ForeignKey(PostCard, on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return f"Photo for postcard_id: {self.postcard_id} @{self.url}"
-
-
-
